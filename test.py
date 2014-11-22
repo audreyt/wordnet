30f3a438f4b7c8acc6d5211755d977f5610c6476
mhshih <d00142002@ntu.edu.tw>
2014-11-22 14:59:05 +0800

    test similarity

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from nltk.corpus import wordnet
-<<<<<<< HEAD
 from ckip import seg
 
 def similar(def1,def2):
@@ -13,7 +12,3 @@ synsets=list(wordnet.all_synsets())
 def1=seg(synsets[0].definition)
 def2=seg(synsets[1].definition)
 print ' '.join(def1),' '.join(def2),similar(def1,def2)
-=======
-for synset in wordnet.all_synsets():
-    print synset.name().encode('utf8'),synset.definition().encode('utf8')
->>>>>>> 52e350b76f1d129f9c1f71e70c0831cd277fc8ea
